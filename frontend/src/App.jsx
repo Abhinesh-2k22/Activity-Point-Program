c887ad4a752264ebdd478f4e7398247a0a5ec47b
AkshayAS-06 <22n206@psgtech.ac.in>
2025-03-23 20:21:01 +0530

    service updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from './Pages/Signup'
 import Landingpage from './Pages/Landingpage'
 import Getrentinghouse from './Pages/Getrentinghouse'
 import GetserviceProvider from './Pages/GetserviceProvider'
+import GetSaloon from './Pages/GetSaloon'
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
           <Route path='/landingPage' Component={Landingpage}/>
           <Route path='/getrenting' Component={Getrentinghouse}/>
           <Route path='/getservice' Component={GetserviceProvider}/>
+          <Route path='/getsaloon' Component={GetSaloon}/>
         </Routes>  
       </div>
     </Router>
